a8cff025636a026acbcc143fa5e3b8e8b1a582ec
Nazarii Romankiv <nazarii_romankiv@epam.com>
2023-04-14 01:12:41 +0300

    feat: implement bare template

--- src/common/expensive-object/expensive-connection.ts
+++ src/common/expensive-object/expensive-connection.ts
@@ -1,8 +1,10 @@
 import {Config} from "../config/types";
+import {inject} from "inversify";
+import {CONFIG} from "../config/di";
 
 // This entity just emulates an expensive object, like connection that should be re-used
 export class ExpensiveConnection {
-    constructor(config: Config) {
+    constructor(@inject(CONFIG) config: Config) {
     }
 
     async connect() {
